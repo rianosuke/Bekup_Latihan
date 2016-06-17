d6ceac9e4b6b7c4e75863a8761eaa8b2c95a1811
Owner <Owner>
2016-06-17 17:37:36 +0700

    test

---
+++ app/src/androidTest/java/id/dilo/bekup_latihan/ApplicationTest.java
@@ -0,0 +1,13 @@
+package id.dilo.bekup_latihan;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
